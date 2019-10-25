a729143081b0685a62be996e961c57f997ed61c3
root <flaxed@users.noreply.github.com>
2019-10-25 01:52:42 +0000

    Updated build scripts

---
+++ scripts/vsremote/build_csharp_files.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+ABSOLUTE_DIR="$(dirname $ABSOLUTE_PATH)"
+ROOT_DIR="$(realpath "$ABSOLUTE_DIR/../..")"
+
+mkdir -p "$ROOT_DIR/build/spec"
+java -Xmx500M -cp "/usr/local/lib/antlr-${ANTLR_VERSION}-complete.jar:$CLASSPATH" org.antlr.v4.Tool \
+     -o "$ROOT_DIR/artifacts/antlr-csharp" \
+     -Dlanguage=CSharp \
+     -package TheToolsmiths.Ddl.Parser \
+     -visitor \
+     -no-listener \
+     "$ROOT_DIR/spec/Ddl.g4"
