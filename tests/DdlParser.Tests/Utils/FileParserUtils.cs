fe5ce03198aad5674e78670298e27c26c6de8034
root <flaxed@users.noreply.github.com>
2019-10-28 02:49:43 +0000

    Reestructured C# test project

---
+++ tests/DdlParser.Tests/Utils/FileParserUtils.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Antlr4.Runtime;
+
+namespace TheToolsmiths.Ddl.Parser.Tests.Utils
+{
+    public static class FileParserUtils
+    {
+        public static DdlParser CreateParserFromPath(string path)
+        {
+            path = Path.Join(PathConstants.ExamplesFolder, path);
+
+            var inputStream = CharStreams.fromPath(path);
+            var lexer = new DdlLexer(inputStream);
+            var commonTokenStream = new CommonTokenStream(lexer);
+            var parser = new DdlParser(commonTokenStream)
+            {
+                BuildParseTree = true
+            };
+
+            return parser;
+        }
+    }
+}
