018b6b353ea59e6191a0747b9a75b663555e311a
Tiago <flaxed@users.noreply.github.com>
2020-01-07 20:41:36 +0000

    Started implementing CLI actions support

--- src/Ddl.Transpiler.Cli/Program.cs
+++ src/Ddl.Transpiler.Cli/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
 using System.Threading.Tasks;
 using Ddl.Transpiler;
 using TheToolsmiths.Ddl.Parser;
@@ -7,11 +10,41 @@ namespace TheToolsmiths.Ddl.Transpiler.Cli
 {
     public static class Program
     {
-        private const string TestPath = "allFeatures.ddl";
-
-        public static async Task Main()
+        public static async Task Main(string[] args)
         {
-            var contents = await DdlTextParser.ReadFromFile(TestPath);
+            var transpileCommand = new Command("transpile")
+            {
+                new Argument<FileInfo>
+                {
+                    Name = "input", Arity = ArgumentArity.ExactlyOne, Description = "Input ddl file"
+                },
+                new Option(new[] {"--output", "-o"})
+                {
+                    Name = "output", Description = "Optional output json file", Argument = new Argument<FileInfo>()
+                }
+            };
+
+            transpileCommand.Handler = CommandHandler.Create<FileInfo, FileInfo>(TranspileFromFilePath);
+
+            var rootCommand = new RootCommand
+            {
+                transpileCommand
+            };
+
+            rootCommand.Name = "ddl";
+            rootCommand.Description = "ddl cli tool";
+
+            await rootCommand.InvokeAsync(args);
+        }
+
+        private static async Task TranspileFromFilePath(FileInfo input, FileInfo? output)
+        {
+            var contents = await DdlTextParser.ReadFromFile(input.FullName);
+
+            if (output != null)
+            {
+                throw new ArgumentException(nameof(output));
+            }
 
             string result = await DdlTranspiler.TranspileToString(contents);
 
